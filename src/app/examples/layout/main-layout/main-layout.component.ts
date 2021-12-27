8cf7d3f700845cad7f3b0bcbfacefefb6f962c0e
serjKim <serjKimChanju@gmail.com>
2021-12-27 23:08:40 +0300

    Sticky sidenav and header

--- src/app/examples/layout/main-layout/main-layout.component.ts
+++ src/app/examples/layout/main-layout/main-layout.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { SidenavCollapseService } from './sidenav-collapse.service';
 
 @Component({
@@ -10,6 +11,10 @@ import { SidenavCollapseService } from './sidenav-collapse.service';
   providers: [SidenavCollapseService],
 })
 export class MainLayoutComponent {
+  public readonly mainCollapseCssClass$ = this.sidenavCollapse.collapsed.pipe(
+    map((result) => (result.collapsed ? 'collapsed' : 'expanded')),
+  );
+
   constructor(public readonly sidenavCollapse: SidenavCollapseService) {}
 
   public onCollapse(collapsed: boolean): void {
