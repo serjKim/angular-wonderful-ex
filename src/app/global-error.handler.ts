4fe676ce7efd834409b1a10b2ad74a74a1fffedc
serjKim <serjKimChanju@gmail.com>
2022-01-07 16:55:41 +0300

    Allow refresh page

--- src/app/global-error.handler.ts
+++ src/app/global-error.handler.ts
@@ -1,5 +1,12 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { ErrorHandler, Injectable } from '@angular/core';
+import { ErrorHandler, Inject, Injectable, InjectionToken, NgZone } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { take } from 'rxjs';
+
+const LOCATION = new InjectionToken<Location>('window.location accessor', {
+  providedIn: 'root',
+  factory: () => window.location,
+});
 
 interface PromiseError {
   readonly rejection: unknown;
@@ -7,21 +14,29 @@ interface PromiseError {
 
 @Injectable()
 export class GlobalErrorHandler extends ErrorHandler {
+  constructor(
+    private readonly snackBar: MatSnackBar,
+    @Inject(LOCATION) private location: Location,
+    private readonly ngZone: NgZone,
+  ) {
+    super();
+  }
+
   public handleError(error: unknown): void {
     const err = this.extractError(error);
     if (err instanceof HttpErrorResponse) {
       switch (err.status) {
         case 401:
         case 403:
-          // redirect
+          this.openSnackBar('Unauthorized. Refresh the page and try again or contact to administrators.');
           break;
         case 404:
         case 400:
-          // display toast
+          this.openSnackBar('Resource not found. Refresh the page and try again or contact to administrators.');
           break;
       }
     } else {
-      // other errors
+      this.openSnackBar('An error occurred. Refresh the page and try again or contact to administrators.');
     }
     super.handleError(error);
   }
@@ -32,5 +47,15 @@ export class GlobalErrorHandler extends ErrorHandler {
 
   private isPromiseError(err: unknown): err is PromiseError {
     return (err as PromiseError)?.rejection != null;
+  }
+
+  private openSnackBar(message: string): void {
+    this.ngZone.run(() => {
+      this.snackBar
+        .open(message, 'Refresh')
+        .onAction()
+        .pipe(take(1))
+        .subscribe(() => this.location.reload());
+    });
   }
 }
