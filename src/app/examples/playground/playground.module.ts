cfc011426d41e4d45290905af6106abe4a8e687d
serjKim <serjKimChanju@gmail.com>
2022-03-22 13:03:35 +0300

    Fix line height for open sans

--- src/app/examples/playground/playground.module.ts
+++ src/app/examples/playground/playground.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { CoreModule } from '../../core';
 
 import { PlaygroundComponent } from './playground.component';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, CoreModule],
+  imports: [CommonModule, FormsModule, CoreModule, MatInputModule, MatFormFieldModule],
   exports: [PlaygroundComponent],
   declarations: [PlaygroundComponent],
   providers: [],
